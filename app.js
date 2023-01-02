76f059d1e49ca9bea17e6c5c6d2aa23ec3fed36c
Silas Morales <silas.mora22@gmail.com>
2023-01-02 12:21:15 -0800

    exercise complete

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Silas"
+let lastName = "Morales"
+var age = 32
+
 // Exercise 2
 
+let language = "Javascript"
+let createdYear = 1995
+let isCaseSentitive = true
+
 // Exercise 3
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+
